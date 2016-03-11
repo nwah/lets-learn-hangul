8b86ed98c795db4852dd3ab4b2efb2c833b2562b
Noah Burney <noah.burney@gmail.com>
2016-03-10 23:07:28 -0600

    build pipeline: use bro, add sass, use public dir

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,29 +1,35 @@
 import gulp from 'gulp';
-import browserify from 'browserify';
-import babelify from 'babelify';
-import fs from 'fs';
-import path from 'path';
-import mkdirp from 'mkdirp';
+import bro from 'gulp-bro';
+import sass from 'gulp-sass';
+import rename from 'gulp-rename';
 import watch from 'gulp-watch';
 
-gulp.task('js:build', () => {
-  mkdirp.sync('.tmp/js');
-  return browserify('app/index.js')
-    .transform(babelify)
-    .bundle()
-    .pipe(fs.createWriteStream('.tmp/js/app.js'));
-});
+gulp.task('js', () =>
+  gulp.src('app/index.js')
+    .pipe(bro({
+      transform: ['babelify'],
+      debug: true
+    }))
+    .pipe(rename('app.js'))
+    .pipe(gulp.dest('public/js'))
+);
 
-gulp.task('js:copy-dist', ['js:build'], () => {
-  mkdirp.sync('dist/js');
-  return gulp.src(['.tmp/js/*.js'])
-    .pipe(gulp.dest('./dist/js/'));
-});
+gulp.task('css', () =>
+  gulp.src('app/styles/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename('app.css'))
+    .pipe(gulp.dest('public/css'))
+);
 
-gulp.task('js', ['js:copy-dist']);
+gulp.task('copyassets', () =>
+  gulp.src('assets/**/*.*')
+    .pipe(gulp.dest('public'))
+);
 
-gulp.task('watch', () => {
+gulp.task('watch', ['js', 'css'], () => {
   watch('app/**/*.js', () => gulp.start('js'));
+  watch('app/**/*.scss', () => gulp.start('css'));
+  watch('assets/**/*.*', () => gulp.start('copyassets'));
 });
 
-gulp.task('default', ['js']);
+gulp.task('default', ['js', 'css', 'copyassets']);
