b0cd226995229125a502864958e07ece7125f13d
Noah Burney <noah.burney@gmail.com>
2016-07-23 23:30:28 -0500

    tweak punctuation styling

--- app/components/BigText.js
+++ app/components/BigText.js
@@ -3,7 +3,7 @@ import { getShapeID } from '../utils/display';
 
 const BigText = ({text, className}) => (
   <svg viewBox="0 0 400 600" className={className || "big-text"}>
-    <text x="200" y="300" dy="250" textAnchor="middle" fontSize="500pt" fontWeight="300" fontFamily="ff-enzo-web">
+    <text x="200" y="300" dy="175" fill="#2d2b29" textAnchor="middle" fontSize="450pt" fontWeight="600" fontFamily="ff-enzo-web">
       {text}
     </text>
   </svg>
